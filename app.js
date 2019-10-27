6f326e7597aa893f1ef72ccd6649a52f3edf7012
finnthedawg <42992404+finnthedawg@users.noreply.github.com>
2019-10-26 20:52:28 -0400

    Pagination for feed page

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var express = require('express'),
   language = require('@google-cloud/language');
 
 
-  // Global app object
+// Global app object
 var app = express();
 
 var server = require('http').createServer(app)
@@ -30,101 +30,105 @@ app.get('/', function (req, res) {
 
 var conf = require('./config/config.json')
 
-app.get('/auth', function(req, res) {
- 
-    // we don't have a code yet
-    // so we'll redirect to the oauth dialog
-    if (!req.query.code) {
-      console.log("Performing oauth for some user right now.");
-    
-      var authUrl = graph.getOauthUrl({
-          "client_id":     conf.client_id
-        , "redirect_uri":  conf.redirect_uri
-        , "scope":         conf.scope
-      });
-   
-      if (!req.query.error) { //checks whether a user denied the app facebook login/permissions
-        res.redirect(authUrl);
-      } else {  //req.query.error == 'access_denied'
-        res.send('access denied');
-      }
-    }
-    // If this branch executes user is already being redirected back with 
-    // code (whatever that is)
-    else {
-      console.log("Oauth successful, the code (whatever it is) is: ", req.query.code);
-      // code is set
-      // we'll send that and get the access token
-      graph.authorize({
-          "client_id":      conf.client_id
-        , "redirect_uri":   conf.redirect_uri
-        , "client_secret":  conf.client_secret
-        , "code":           req.query.code
-      }, function (err, facebookRes) {
-        res.redirect('/LoggedIn');
-      });
-    }
-  });
+app.get('/auth', function (req, res) {
 
-  let getSentiment = async function (text) {
-  
-    // Instantiates a client
-    const client = new language.LanguageServiceClient();
-  
-    const document = {
-      content: text,
-      type: 'PLAIN_TEXT',
-    };
-  
-    // Detects the sentiment of the text
-    const [result] = await client.analyzeSentiment({document: document});
-    const sentiment = result.documentSentiment;
+  // we don't have a code yet
+  // so we'll redirect to the oauth dialog
+  if (!req.query.code) {
+    console.log("Performing oauth for some user right now.");
 
-    return({
-        "text" : text,
-        "score" : sentiment.score,
-        "magnitude" : sentiment.magnitude,
+    var authUrl = graph.getOauthUrl({
+      "client_id": conf.client_id
+      , "redirect_uri": conf.redirect_uri
+      , "scope": conf.scope
     });
+
+    if (!req.query.error) { //checks whether a user denied the app facebook login/permissions
+      res.redirect(authUrl);
+    } else {  //req.query.error == 'access_denied'
+      res.send('access denied');
+    }
+  }
+  // If this branch executes user is already being redirected back with 
+  // code (whatever that is)
+  else {
+    console.log("Oauth successful, the code (whatever it is) is: ", req.query.code);
+    // code is set
+    // we'll send that and get the access token
+    graph.authorize({
+      "client_id": conf.client_id
+      , "redirect_uri": conf.redirect_uri
+      , "client_secret": conf.client_secret
+      , "code": req.query.code
+    }, function (err, facebookRes) {
+      res.redirect('/LoggedIn');
+    });
+  }
+});
+
+let getSentiment = async function (text) {
+
+  // Instantiates a client
+  const client = new language.LanguageServiceClient();
+
+  const document = {
+    content: text,
+    type: 'PLAIN_TEXT',
+  };
+
+  // Detects the sentiment of the text
+  const [result] = await client.analyzeSentiment({ document: document });
+  const sentiment = result.documentSentiment;
+
+  return ({
+    "text": text,
+    "score": sentiment.score,
+    "magnitude": sentiment.magnitude,
+  });
 }
 
 
-  app.get('/LoggedIn', function (req, res){
-
-    graph.get("/me/feed", function(err, res){
-        if (err) {
-            console.log(err);
-        } else {
-            let messages = res.data.filter(ele => ele.message !== undefined);
-
-
-
-            let sentiments = [];
-            messages.forEach(ele => {
-                sentiments.push(getSentiment(ele.message));
-            });
-            Promise.all(sentiments).then(function(values) {
-                console.log(values);
-            })
-        }
+async function getFeed(queryPath) {
+  let newMessages;
+  let next;
+  let feedPromise = new Promise((resolve, reject) => {
+    graph.get(queryPath, function (err, res) {
+      if (err) {
+        console.log(err);
+      } else {
+        newMessages = res.data.filter(ele => ele.message !== undefined);
+        next = res.paging.next;
+        resolve();
+      }
     });
-
-    // graph.get("/me/photos", function (err, res){
-    //     if (err){
-    //         console.log(err);
-    //     } else {
-    //         photoIds = res.data.map(ele => {return ele.id});
-    //         photoIds.forEach(ele => {
-    //             graph.get(ele + "/comments", function (err, res) {
-    //                 console.log(res);
-    //             });
-    //         })
-    //         console.log(photoIds);
-    //     }
-    // })
-    res.render('LoggedIn', {});
   });
+  await feedPromise;
+  return({
+    "Messages" : newMessages,
+    "Next" : next,
+  });
+}
+
+app.get('/LoggedIn', async function (req, res) {
+  let messages = [];
+  let nextPage = "/me/feed";
+  let reachedEnd = false;
+  while (reachedEnd === false) {
+    let val = await getFeed(nextPage);
+    console.log(val.next);
+    if(val.next === undefined){
+      reachedEnd = true;
+    } else {
+      nextPage = val.next
+    }
+    console.log(val);
+  }
+
+  console.log("Done");
+  res.render('LoggedIn', {});
+});
 
 
 server.listen(process.env.PORT || 3000, function () {
-    console.log('Listening on port ' + server.address().port);
-  });
+  console.log('Listening on port ' + server.address().port);
+});
